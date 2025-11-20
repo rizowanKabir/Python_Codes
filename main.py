0a8621b764e5d61bb9063f3b6717296c1c8993bc
Rizowan Kabir <sohagrizowan@gmail.com>
2025-11-20 13:55:06 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,59 +1,70 @@
-# ATM Simulation
+#Tuple, Dictionary & Set
+l = []
+for i in range(1,11):
+    l.append(i)
+print(l)
 
-menu = input(""" 
-Hi there!! Welcome to ATM Simulation
-Please enter what option you want
+#Tuple comprehensive
 
-1. Enter 1 for pin change
-2. Enter 2 for balance check
-3. Enter 3 for withdraw 
-4. Enter 4 for Deposite 
-5. Enter 5 for Exit
-""")
-print(menu)
+ print(tuple(i for i in range(1, 11)))
 
-if menu == "1":
-  print("Please change your pin")
-elif menu == "2":
-  print("Here you can check your balance")
-elif menu == "3":
-  print("Here you can withdraw")
-elif menu == "4":
-  print("Here you can Deposit")
+# Dictionary
+
+d1 = {"name":"Alex","age":20, "Gender":"male"}
+d1 ["name"] = "Shagor"
+print(d1)
+
+d1 = {"name":"Alex","age":20, "Gender":"male"}
+
+for i in d1:
+    print(f"{i}:{d1[i]}")
+
+# Using if condition
+
+products = {"phone":10, "laptop":12, "Charger":100, "Headphone":0}
+
+{key:value for (key,value) in products.items() if value > 0}
+print(products)
+
+# Restaurant Management System
+
+menu = {
+    "Coffe": 200,
+    "Pasta": 150,
+    "Pizza": 400,
+    "Burger": 300,
+}
+
+print("------WELCOME TO THE RIZOWAN RESTAURANT------")
+print(
+    """
+    coffe: 200 tk
+    Pasta: 150 tk
+    Pizza: 400 tk
+    Burger: 300 tk
+    """
+)
+
+item1 = input("Enter the item what you want order: ")
+total_price = 0
+if item1 in menu:
+    total_price += menu[item1]
+    print(f"Your order is {item1} & your total bill is {total_price} TK")
 else:
-  print("Exit")
+    print("Invalid Item. Please try order from our menu")
 
-# Find the minimum number
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-num3 = int(input("Enter the third number: "))
-
-if num1 < num2 and num1 < num3:
-  print(f"Minimum number is: {num1}")
-elif num2 < num1 and num2 < num3:
-  print(f"Minimum number is: {num2}")
-else:
-  print(f"Minimum number is: {num3}")
-
-#Login function
-email = input("Enter the email here: ")
-password = input("Enter the password here: ")
-
-actual_email = "sohagrizowan@gmail.com"
-actual_password = "kabir001"
-if email == actual_email and password == actual_password:
-  print("Login Successfull !!")
-elif email == actual_email and password != actual_password:
-  print("Please try again !!")
-  password = input("Enter the password again: ")
-
-  if password == actual_password:
-    print("Login Succefull!!")
-  else:
-    print("Wrong!! Password")
+another_item = input("Do you want add another item?(Yes/No): ").lower()
+if another_item == "yes":
+    item2 = input("Enter the second order what you want: ")
+    if item2 in menu:
+        total_price += menu[item2]
+        print(f"You ordered {item2} & your total ordered is {total_price} TK")
+    else:
+        print("Invalid Item. Please try to order from our menu")
+print(f"Your total amount is {total_price} TK. Thank You")
 
 
-else:
-  print("Wrong Crdential. Try right one please!!")
+
+
 
 
